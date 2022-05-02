4418e83fabb3be3285b87cf77f5ec2710371a99b
Joona Holappa <joona.holappa@gmail.com>
2022-05-02 15:02:07 +0300

    minor changes

--- locationDistribution.py
+++ locationDistribution.py
@@ -3,23 +3,32 @@ from textwrap import indent
 
 dic = {}
 
-file = open("dataFile.json")
-dataFile = json.load(file)
- 
-for i in dataFile:
-    if dataFile[i]["Rep"].startswith("\nLocation: "):
-        string = dataFile[i]["Rep"]
-        string = string[11:]
-        for chr in string:
-            if chr == "\n":
-                temp = string.index(chr)
-        loc = string[0 : temp]
-        try:
-            dic[loc] = dic[loc] + 1
-        except KeyError:
-            dic[loc] = 1
-        
+def openFile(fileName):
+    file = open(fileName)
+    dataFile = json.load(file)
+    return dataFile
 
-print(dic)
-with open('locationData.json', 'w') as saveFile:
-    json.dump(dic, saveFile, indent=4)
+def createDistribution(dataFile): 
+    for i in dataFile:
+        if dataFile[i]["Rep"].startswith("\nLocation: "):
+            string = dataFile[i]["Rep"]
+            string = string[11:]
+            for chr in string:
+                if chr == "\n":
+                    temp = string.index(chr)
+            loc = string[0 : temp]
+            try:
+                dic[loc] = dic[loc] + 1
+            except KeyError:
+                dic[loc] = 1
+    return dic
+
+def saveFile(dictionary):
+    with open('locationData.json', 'w') as saveFile:
+        json.dump(dictionary, saveFile, indent=4)
+    print("made it here")
+
+if __name__ == "__main__":
+    openedData = openFile("dataFile.json")
+    createDistribution(openedData)
+    saveFile(dic)
